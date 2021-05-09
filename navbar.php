ee70ecf68d9832e424a5686aaa2d508c5dc4df52
B Sarina <63443233+119CS0098@users.noreply.github.com>
2021-05-09 19:04:50 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -9,6 +9,5 @@
                     <li><a href="createuser.php">Create User</a></li>
                     <li><a href="transfermoney.php">Transfer Money</a></li>
                     <li><a href="transactionhistory.php">Transaction History</a></li>
-
             </div>
-        </div>
+</div>
